fb4602695b179ccb0cac3afd8bae4eb9deab3115
Quality.kun <jrgames@kaiohana.net>
2019-10-02 21:47:55 -0700

    code4 new code, recursive

---
+++ code4.js
@@ -0,0 +1,50 @@
+const org1_depts=[
+    {
+        name: 'accounting',
+        children: [
+            {name: 'accounting payable', children: []},
+            {name: 'accounting receivable', children: []},
+        ],
+    },
+    {
+        name: 'finance',
+        children: [],
+    },
+]
+
+const org2_depts=[
+    {
+        name: 'accounting',
+        children: [
+            {name: 'accounting payable', children: []},
+            {
+                name: 'accounting receivable',
+                children: [{name: 'cash', children: []}, {name: 'check', children: []}],
+            },
+        ],
+    },
+    {
+        name: 'finance',
+        children: [{name: 'investment', children: []}],
+    },
+]
+
+const start_tag = '<ul>'
+const end_tag = '</ul>'
+const start_li = '<li>'
+const end_li = '</li>'
+
+function PrintDepts(depts) {
+    output = start_tag;
+    for (let i = 0; i < depts.length; i++) {
+        let x = depts[i];
+        output += start_li+x.name+end_li
+        if (x.children !== []) {
+            PrintDepts(x.children);
+        }
+        output += end_tag;
+        }
+    }
+
+PrintDepts(org1_depts);
+PrintDepts(org2_depts);
